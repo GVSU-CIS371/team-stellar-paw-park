64a755002eceb195ff28186b23196fb1d0d0aeda
slomac1 <slomac@mail.gvsu.edu>
2026-04-20 07:04:07 -0400

    moved user login/logout handlers to authStore and added auth listener

--- Stellar Paw Park/src/stores/authStore.ts
+++ Stellar Paw Park/src/stores/authStore.ts
@@ -1,14 +1,24 @@
 import { defineStore } from 'pinia'
-import { appUser } from '../types/storeTypes'
-import db from '../firebase'
-import { DocumentReference, setDoc, doc, getDoc, DocumentSnapshot } from 'firebase/firestore'
-import { User } from 'firebase/auth'
+import { 
+    User,
+    GoogleAuthProvider, 
+    signInWithPopup, 
+    getAuth, 
+    signInWithEmailAndPassword, 
+    createUserWithEmailAndPassword, 
+    onAuthStateChanged,
+    signOut
+} from 'firebase/auth'
+import { auth } from '../firebase'
+import { useRouter } from 'vue-router';
+import { useUserStore } from './userStore';
+
 
 export const useAuthStore = defineStore('AuthStore', {
     state: () => ({
         signUp: false,
         user: null as User | null,
-        userData: null as appUser | null,
+        authListener: null as (() => void) | null,
         email: "",
         password1: "",
         password2: "",
@@ -17,39 +27,78 @@ export const useAuthStore = defineStore('AuthStore', {
         message: "",
     }),
     actions: {
-        setUser(user: User | null) {
-            this.user = user;
-            if (user) {
-                const userDoc: DocumentReference = doc(db, 'users', user.uid);
-                getDoc(userDoc).then((qd: DocumentSnapshot) => {
-                    if (qd.exists()) {
-                        this.userData = qd.data() as appUser;
+        authListenerInit() {
+            if (this.authListener) return;
+
+            const router = useRouter();
+
+            this.authListener = onAuthStateChanged(auth, (user) => {
+                this.user = user;
+
+                const userStore = useUserStore();
+                userStore.clearUser();
+
+                if (user) {
+                    userStore.setUser(user);
+                    router.push('/userPage');
+                }
+                else {
+                    userStore.clearUser();
+                    if (router.currentRoute.value.path === '/userPage') {
+                        router.push('/');
                     }
-                    else {
-                        this.createUserData(user);
-                    }
+                }
+            })
+        },
+        loginWithGoogle() {
+            const provider = new GoogleAuthProvider();
+            signInWithPopup(getAuth(), provider)
+                .then((result) => {
+                    this.message = "Google login successful!";
+                    this.reset();
                 })
-            }
-            else {
-                this.userData = null;
-            }
+                .catch((error) => {
+                    this.message = "Google login failed. Please try again.";
+                });
         },
-        createUserData(user: User) {
-            const userDoc: DocumentReference = doc(db, 'users', user.uid);
-            const userData: appUser = {
-                userId: user.uid,
-                fname: user.displayName?.split(' ')[0] || this.fname,
-                lname: user.displayName?.split(' ')[1] || this.lname,
-                email: user.email || this.email,
-                phone: '',
-                admin: false
-            };
-            setDoc(userDoc, {userData
-            }).then(() => {
-                console.log('User data created successfully');
-            }).catch((error) => {
-                console.error('Error creating user data:', error);
-            });
+        loginWithEmail() {
+            signInWithEmailAndPassword(getAuth(), this.email, this.password1)
+                .then((userCredential) => {
+                    this.message = "Login successful!";
+                    this.reset();
+                })
+                .catch((error) => {
+                    this.message = "Login failed. Please check your credentials.";
+                });
         },
+        registerWithEmail() {
+            if (!this.password1 || !this.password2 || !this.email || !this.fname || !this.lname) {
+                this.message = "Please fill in all fields.";
+                return;
+            }
+            if (this.password1 !== this.password2) {
+                this.message = "Passwords do not match. Please try again.";
+                return;
+            }
+
+            createUserWithEmailAndPassword(getAuth(), this.email, this.password1)
+                .then(async (result) => {
+                    this.message = "Registration successful";
+                    this.signUp = false;
+                    this.reset();
+                })
+                .catch((error) => {
+                    this.message = "Failed to register. Try logging in.";
+                });
+        },
+        async signOut() {
+            this.reset();
+            await signOut(auth);
+        },
+        reset() {
+            this.email = "";
+            this.password1 = "";
+            this.password2 = "";
+        }
     },
 })
--- Stellar Paw Park/src/views/userLogin.vue
+++ Stellar Paw Park/src/views/userLogin.vue
@@ -17,9 +17,9 @@
       </v-card-text>
       <p>{{ authStore.message }}</p>
       <v-card-actions>
-        <v-btn variant="tonal" @click="loginWithEmail">Login</v-btn>
+        <v-btn variant="tonal" @click="authStore.loginWithEmail()">Login</v-btn>
         <v-btn variant="tonal" @click="authStore.signUp = true">Sign Up</v-btn>
-        <v-btn variant="tonal" @click="loginWithGoogle">Login with Google</v-btn>
+        <v-btn variant="tonal" @click="authStore.loginWithGoogle()">Login with Google</v-btn>
       </v-card-actions>
     </v-card>
       <v-card v-else width="400" class="pa-4 login-card">
@@ -36,7 +36,7 @@
         <p>{{ authStore.message }}</p>
         <v-card-actions>
           <v-btn variant="tonal" @click="authStore.signUp = false">Back to Login</v-btn>
-          <v-btn variant="tonal" @click="registerWithEmail">Sign Up</v-btn>
+          <v-btn variant="tonal" @click="authStore.registerWithEmail()">Sign Up</v-btn>
         </v-card-actions>
       </v-card>
   </v-container>
@@ -44,53 +44,9 @@
 
 <script setup lang="ts">  
   import logo from '../assets/images/StellarParkThree.png'
-  import { GoogleAuthProvider, signInWithPopup, getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
   import { useAuthStore } from '../stores/authStore';
-  import { useRouter } from 'vue-router';
   const authStore = useAuthStore();
-  const router = useRouter();
-
-  function loginWithGoogle() {
-    const provider = new GoogleAuthProvider();
-    signInWithPopup(getAuth(), provider)
-      .then((result) => {
-        authStore.message = "Google login successful";
-        authStore.setUser(result.user);
-        router.push('/userPage');
-      })
-      .catch((error) => {
-        authStore.message = "Google login failed. Please try again.";
-      });
-  }
-  function loginWithEmail() {
-    signInWithEmailAndPassword(getAuth(), authStore.email, authStore.password1)
-      .then((result) => {
-        authStore.message = "Login successful";
-        authStore.setUser(result.user);
-        router.push('/userPage');
-      })
-      .catch((error) => {
-        authStore.message = "Failed to login. Please check your email and password or register.";
-      });
-  }
-  function registerWithEmail() {
-    if (!authStore.password1 || !authStore.password2 || !authStore.email || !authStore.fname || !authStore.lname) {
-      authStore.message = "Please fill in all fields.";
-      return;
-    }
-    if (authStore.password1 !== authStore.password2) {
-      authStore.message = "Passwords do not match. Please try again.";
-      return;
-    }
-    createUserWithEmailAndPassword(getAuth(), authStore.email, authStore.password1)
-      .then((result) => {
-        authStore.message = "Registration successful";
-        authStore.setUser(result.user);
-      })
-      .catch((error) => {
-        authStore.message = "Failed to register. Try logging in.";
-      });
-  }
+  authStore.authListenerInit();
 
 </script>
 
