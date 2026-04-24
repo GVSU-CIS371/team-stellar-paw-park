1d26e51804d1d2b678f71b910fb7d1490d457564
slomac1 <slomac@mail.gvsu.edu>
2026-04-24 02:36:50 -0400

    Admin page started with vaccination verification data table

---
+++ Stellar Paw Park/src/stores/adminStore.ts
@@ -0,0 +1,63 @@
+import { defineStore } from 'pinia'
+import { userType, dogType } from '../types/storeTypes'
+import db from '../firebase'
+import {  
+    QuerySnapshot, 
+    collection,
+    CollectionReference, 
+    doc, 
+    getDocs, 
+    updateDoc,
+    query,  
+    where,
+    onSnapshot,
+    QueryDocumentSnapshot,
+} from 'firebase/firestore';
+
+export const useAdminStore = defineStore('AdminStore', {
+    state: () => ({
+        verifyVaccination: [] as dogType[],
+        dogListener: null as (() => void) | null,
+        headers: [
+            {title: "Name", key: 'name'},
+            {title: "Breed", key: 'breed'},
+            {title: "Color", key: 'color'},
+            {title: "DOB", key: 'DOB'},
+            {title: "Vaccination Status", key: 'vaccinated'},
+            {title: "Vaccination Form", key: 'vaccinationImg'},
+            {title: "Approve", key: 'actions', sortable: false},
+        ]
+    }),
+    actions: {
+        initDogListener() {
+            this.verifyVaccination = [];
+            if (this.dogListener) {
+                this.dogListener();
+                this.dogListener = null;
+            }
+
+            const dogColl = query(collection(db, 'dogs'), where('vaccinated', '==', false ));
+            this.dogListener = onSnapshot(dogColl, async (s: QuerySnapshot) => {
+                for (let change of s.docChanges()) {
+                    const dogData = change.doc.data() as dogType;
+                    if (change.type === 'added') {
+                        this.verifyVaccination.push({...dogData, id: change.doc.id})
+                    }
+                    else if (change.type === 'removed'){
+                        this.verifyVaccination = this.verifyVaccination.filter((i) => i.id === dogData.id)
+                    }
+                }
+            })
+        },
+        updateVaccination(dog: dogType) {
+            const dogColl: CollectionReference = collection(db, 'dogs');
+            const qr = query(dogColl, where("ownerId", "==", dog.ownerId), where("name", "==", dog.name));
+            getDocs(qr).then((qs: QuerySnapshot) => {
+                qs.forEach(async (qd: QueryDocumentSnapshot) => {
+                    const dogDoc = doc(db, 'dogs', qd.id);
+                    await updateDoc(dogDoc, {vaccinated: true})
+                })
+            })
+        }
+    },
+});
--- Stellar Paw Park/src/views/admin.vue
+++ Stellar Paw Park/src/views/admin.vue
@@ -1,6 +1,38 @@
 <template>
-  <main>
-    <h1>Admin</h1>
-    <p>Run the business</p>
-  </main>
+  <v-container>
+    <div class="d-flex">
+
+      <v-navigation-drawer permanent width="200">
+        <v-list-item href="#vaccinations" title="Vaccinations"/>
+        <v-list-item href="#bookings" title="Bookings" />
+      </v-navigation-drawer>
+
+      <div class="flex-grow-1 pa-4">
+
+        <section id="vaccinations">
+          <h1>Verify Vaccination Records</h1>
+          <v-data-table :items="adminStore.verifyVaccination" :headers="adminStore.headers">
+            <template v-slot:item.DOB="{ item }">
+              {{ item.DOB?.toDate?.().toLocaleDateString?.() }}
+            </template>
+            <template v-slot:item.ownderId="{ item }">
+              {{ item.DOB?.toDate?.().toLocaleDateString?.() }}
+            </template>
+            <template v-slot:item.actions="{ item }">
+            <v-btn variant="tonal" size="md" 
+              @click="adminStore.updateVaccination(item)"
+              >Approve
+            </v-btn>
+          </template>
+          </v-data-table>
+        </section>
+      </div>
+    </div>
+  </v-container>
 </template>
+
+<script setup lang="ts">
+import { useAdminStore } from '../stores/adminStore'
+const adminStore = useAdminStore();
+adminStore.initDogListener();
+</script>
