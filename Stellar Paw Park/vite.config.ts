77704ea53850675070b59c1eadf1d7abb3552d91
slomac1 <slomac@mail.gvsu.edu>
2026-04-24 22:04:10 -0400

    switched to firebase hosting instead of gh-pages

--- Stellar Paw Park/vite.config.ts
+++ Stellar Paw Park/vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/team-stellar-paw-park/",
+  base: "/",
   plugins: [
     vue(),
     vueDevTools(),
