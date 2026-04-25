668b2b0acc3ef4d83901e906e72684b445802feb
slomac1 <slomac@mail.gvsu.edu>
2026-04-25 03:13:49 -0400

    Completely reworked bookings page so that a listener is installed on time slots. Also only querying the database once on slot creation instead of n slot times. updated booking directories as well for ease of access

--- Stellar Paw Park/src/stores/bookingStore.ts
+++ Stellar Paw Park/src/stores/bookingStore.ts
@@ -1,69 +1,71 @@
 import { defineStore } from 'pinia'
-import { areaType, hourType, slotType } from '../types/storeTypes'
+import { areaType, hourType, slotType, userType } from '../types/storeTypes'
 import db from '../firebase'
 import { 
     addDoc, 
     collection, 
-    CollectionReference, 
+    setDoc,
+    updateDoc,
+    increment,
+    arrayUnion,
     doc,
     getDoc,
     getDocs,
-    Query,
-    query,
-    where,
-    QuerySnapshot,
-    QueryDocumentSnapshot,
     DocumentReference,
     DocumentSnapshot,
-    Timestamp,
+    onSnapshot,
 } from 'firebase/firestore';
 
 export const useBookingStore = defineStore('BookingStore', {
     state: () => ({ 
-        areas: [] as areaType[],
-        timeSlots: [] as slotType[],
-        selectedSlot: {} as slotType,
-        selectedDogs: [],
-        booking: false,
-        date: new Date,
-        hours: {} as hourType,
-        user: "",
-        bookedHours: {} as Record<string, number>,
-        errorMessage: '',
+        selectedDate: new Date(),
+        date: '',
         callendarMessage: '',
+        errorMessage: '',
+        showError: false,
+        booking: false,
+        hours: {} as hourType,
+        timeSlots: [] as slotType[],
+        areas: [] as areaType[],
+        bookingUnsubscribe: null as null | (() => void),
+        selectedDogId: [],
+        selectedSlot: {} as slotType,
     }),
     actions: {
-        async dateSelected() {
-            this.timeSlots = [];
-            this.areas = [];
-            if (this.checkSelectedDate()) {
-                this.callendarMessage = "Unable to view past bookings, please select another day."
+        async bookingsInit () {
+            // Pull areas
+            const areasSnapshot = await getDocs(collection(db, 'areas'));
+            this.areas = areasSnapshot.docs.map(doc => ({
+                name:doc.id, 
+                ...(doc.data() as Omit<areaType, "name">)
+            }))
+        },
+        async formatDate() {
+            this.callendarMessage = '';
+            const day = this.selectedDate.getDate();
+            const month = this.selectedDate.getMonth() + 1;
+            const year = this.selectedDate.getFullYear();
+            this.date = year.toString() + "-" + month.toString() + "-" + day.toString();
+            
+            // Check if closed on day selected and update error message
+            if (this.selectedDate.getDay() === 0) {
+                this.callendarMessage = "We are closed on Sunday, please select another day!"
+                this.timeSlots = [];
+                return;
+            }
+            const closeRef = doc(db, "closures", this.date)
+            const qd = await getDoc(closeRef);
+            if (qd.exists()) {
+                this.callendarMessage = "We are closed this day due to " + qd.data().reason;
+                this.timeSlots = [];
                 return;
             }
             this.slotInit();
         },
         async slotInit() {
-            // Check if closed on day selected and update error message
-            const startOfDay = new Date (this.date.setHours(0,0,0,0));
-            const endOfDay = new Date(this.date.setHours(23,59,59,999));
-            const getClose: Query = query(collection(db, "closures"), 
-                where("day", ">=", Timestamp.fromDate(startOfDay)),
-                where("day", "<=", Timestamp.fromDate(endOfDay)));
-            const qs = await getDocs(getClose);
-            if (!qs.empty) {
-                const qd = qs.docs[0];
-                this.callendarMessage = "We are closed this day due to " + qd?.data().reason;
-                return;
-            }
-            
-            // Get the day of the week as an int and pull hours from firebase
-            const selectedDay = this.date.getDay().toString();
-            if (this.date.getDay() === 0) {
-                this.callendarMessage = "We are closed on Sundays, please select another day.";
-                return;
-            }
-            this.callendarMessage = '';
-            const hoursDoc: DocumentReference = doc(db, 'businessHours', selectedDay);
+            this.timeSlots = [];
+            // Pull open hours from firebase
+            const hoursDoc: DocumentReference = doc(db, 'businessHours', this.selectedDate.getDay().toString());
             await getDoc(hoursDoc).then((qd: DocumentSnapshot) => {
                 if (qd.exists()) {
                     this.hours = qd.data() as hourType;
@@ -73,93 +75,90 @@ export const useBookingStore = defineStore('BookingStore', {
                     return;
                 }
             });
-            const areasSnapshot = await getDocs(collection(db, 'areas'));
-            this.areas = areasSnapshot.docs.map(doc => ({
-                name:doc.id, 
-                ...(doc.data() as Omit<areaType, "name">)
-            }))
-            for (let hour = this.hours.start; hour < this.hours.end; hour++) {
-                this.bookedHours = {} as Record<string, number>;
-                await this.checkBookings(hour);
-                for (const area of this.areas) {
-                    this.timeSlots.push({
-                        hour,
-                        name: area.name,
-                        type: area.type,
-                        currentCapacity: this.bookedHours[area.name]?? 0,
-                        maxCapacity: area.capacity
-                    })
-                }
+            // Pull bookings for the selected day
+            const bookingRef = doc(db, "bookingsByDate", this.date);
+            if (this.bookingUnsubscribe) {
+                this.bookingUnsubscribe();
             }
+            this.timeSlots = [];
+            this.bookingUnsubscribe = onSnapshot(bookingRef, (snapshot) => {
+                const bookingData = snapshot.exists()
+                    ? snapshot.data()?.hours ?? {}
+                    : {};
+                const newSlots = [] as slotType[];
+                // Fill in timeslots with empty or pulled bookings
+                for (let hour = this.hours.start; hour < this.hours.end; hour++) {
+                    for (const area of this.areas) {
+                        newSlots.push({
+                            hour,
+                            name: area.name,
+                            type: area.type,
+                            currentCapacity: bookingData?.[hour.toString()]?.[area.name]?.dogCount ?? 0,
+                            maxCapacity: area.capacity
+                        });
+                    }
+                }
+                this.timeSlots = newSlots;
+            })
         },
         formatHour(hour: number): string {
             const suffix = hour >= 12 ? "PM" : "AM"
             const formatted = hour % 12 || 12
             return `${formatted.toString().padStart(2, " ")}:00 ${suffix}`
         },
-        checkSelectedDate() {
-            const todaysDate = new Date();
-
-            const today = new Date(
-                todaysDate.getFullYear(),
-                todaysDate.getMonth(),
-                todaysDate.getDate(),
-            )
-
-            const selectedDate = new Date(
-                this.date.getFullYear(),
-                this.date.getMonth(),
-                this.date.getDate()
-            )
-
-            return selectedDate < today
-        },
-        async checkBookings(hour: number) {
-            const getHourColl: Query = query( collection(db, "bookings"), where("hour", "==", hour), where("date", "==", this.date));
-            await getDocs(getHourColl).then((qs: QuerySnapshot) => {
-                qs.forEach((qd: QueryDocumentSnapshot) => {
-                    const bookData = qd.data();
-                    if (bookData.area in this.bookedHours) {
-                        this.bookedHours[bookData.area] += bookData.dogs.length;
-                    }
-                    else {
-                        this.bookedHours[bookData.area] = bookData.dogs.length;
-                    }
-                })
-            })
-        },
-        addBooking() {
-            const capacityRemaining = this.selectedSlot.maxCapacity - this.selectedSlot.currentCapacity;
-            if (this.selectedSlot.type === "private" && this.selectedSlot.currentCapacity !== 0) {
-                this.errorMessage = "This session is already booked, pick another time slot.";
+        checkBooking(user: string | undefined) {
+            const availableSpace = this.selectedSlot.maxCapacity - this.selectedSlot.currentCapacity;
+            if(!user) {
+                this.errorMessage = "Must be logged in to book a time"
+                this.showError = true;
                 return;
             }
-            else if ((capacityRemaining - this.selectedDogs.length) < 0 && this.selectedSlot.type !== "private") {
-                this.errorMessage = "Not enough space remaining, pick another time slot.";
+            else if(availableSpace <= 0) {
+                this.errorMessage = "This session is full, select another."
+                this.showError = true;
                 return;
             }
-
-            const bookingColl: CollectionReference = collection(db, 'bookings');
-            addDoc(bookingColl, {user: this.user, date: this.date, hour: this.selectedSlot.hour ,area: this.selectedSlot.name, dogs: this.selectedDogs})
-                .then(() => {
-                    console.log("Booking successfully added.")
-                })
-                .catch(() => {
-                    console.log("Failed booking creation.")
-                })
-            this.selectedSlot = {} as slotType;
-            this.selectedDogs = [];
-            this.booking = false;
-        },
-        setupBooking(slot: slotType, user: string) {
-            if (!user) {
+            else if((availableSpace - this.selectedDogId.length) <= 0) {
+                this.errorMessage = "Too many dogs selected, please pick another slot or take a dog off."
+                this.showError = true;
                 return;
             }
-            else {
-                this.user = user;
-            }
-            this.selectedSlot = slot;
             this.booking = true;
+        },
+        async addBooking(user: userType) {
+            const bookingRef = doc(db, "bookingsByDate", this.date);
+            const hourKey = String(this.selectedSlot.hour);
+            const areaKey = this.selectedSlot.name;
+            try {
+                await updateDoc(bookingRef, {
+                    [`hours.${hourKey}.${areaKey}.dogCount`]: increment(this.selectedDogId.length),
+
+                    [`hours.${hourKey}.${areaKey}.dogId`]: arrayUnion(...this.selectedDogId),
+
+                    [`hours.${hourKey}.${areaKey}.userId`]: arrayUnion(user.userId)
+                });
+            } catch (err) {
+                await setDoc(bookingRef, {
+                    hours: {
+                        [hourKey]: {
+                            [areaKey]: {
+                                dogCount: this.selectedDogId.length,
+                                dogId: this.selectedDogId,
+                                userId: [user.userId]
+                            }
+                        }
+                    }
+                }, { merge: true });
+            }
+            
+            const userBookingRef = collection(db, "userBookings", user.userId, "bookings");
+            await addDoc(userBookingRef, 
+                {date: this.date, 
+                time: this.selectedSlot.hour, 
+                dogs: this.selectedDogId,
+                area: this.selectedSlot.name})
+            this.booking = false;
+            this.selectedDogId = [];
         }
     }
 });
--- Stellar Paw Park/src/views/booking.vue
+++ Stellar Paw Park/src/views/booking.vue
@@ -4,8 +4,8 @@
     <v-row class="justify-center">
       <v-date-picker 
       title="Pick a date to view available time slots. Must be logged in to sign up."
-        v-model="bookingStore.date" 
-        @click="bookingStore.dateSelected(); bookingStore.errorMessage = ''"
+        v-model="bookingStore.selectedDate" 
+        @click="bookingStore.formatDate(); bookingStore.errorMessage = ''"
         :weekday-format="$vuetify.display.width > 550 ? 'long' : 'short'"
         width="700"
       ></v-date-picker>
@@ -18,10 +18,10 @@
         :key="index"
         cols="4"
       >
-        <v-card class="mb-4 pa-4" @click="bookingStore.errorMessage = ''; bookingStore.setupBooking(slot, userStore.user?.userId ?? '')">
+        <v-card class="mb-4 pa-4" @click="bookingStore.selectedSlot = slot; bookingStore.checkBooking(userStore.user?.userId)">
           <div class="d-flex align-center">
             <div>
-              <v-card-title class="pa-0"> {{ bookingStore.formatHour(slot.hour) }} </v-card-title>
+              <v-card-title class="pa-0">{{ bookingStore.formatHour(slot.hour) }}</v-card-title>
               <v-card-text class="pa-0">
                 <div>Area Name: {{ slot.name }}</div>
                 <div>Capacity: {{ slot.currentCapacity }} / {{ slot.maxCapacity }}</div>
@@ -34,26 +34,30 @@
     </v-row>
 
   </v-container>
-  <v-dialog v-model="bookingStore.booking" max-width="400">
-    <v-card v-if="bookingStore.errorMessage">
+
+  <v-dialog v-model="bookingStore.showError" max-width="400">
+    <v-card>
       <v-card-title>Error</v-card-title>
       <v-card-text>
         {{ bookingStore.errorMessage }}
       </v-card-text>
     </v-card>
-    <v-card v-else>
+    </v-dialog>
+    <v-dialog v-model="bookingStore.booking" max-width="400">
+    <v-card>
       <v-card-title>Select Dog(s) to add to Booking</v-card-title>
       <v-card-text>
         <v-select 
-        v-model="bookingStore.selectedDogs"
+        v-model="bookingStore.selectedDogId"
         :items="userStore.dogs"
         item-title="name"
+        item-value="id"
         multiple
         />
       </v-card-text>
       <v-card-actions>
-        <v-btn @click="bookingStore.booking=false; bookingStore.selectedDogs=[]">Cancel</v-btn>
-        <v-btn @click="bookingStore.addBooking()">Book</v-btn>
+        <v-btn @click="bookingStore.booking=false; bookingStore.selectedDogId=[]">Cancel</v-btn>
+        <v-btn v-if="userStore.user" @click="bookingStore.addBooking(userStore.user)">Book</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -65,4 +69,5 @@ import { useBookingStore } from '../stores/bookingStore';
 import { useUserStore } from '../stores/userStore';
 const bookingStore = useBookingStore();
 const userStore = useUserStore();
+bookingStore.bookingsInit();
 </script>
